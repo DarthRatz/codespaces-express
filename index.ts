f8a2072f0ef647544e02877f3ff45dd64fe93849
Éamonn Reilly <eamonnreilly87@hotmail.com>
2023-03-26 19:58:12 +0100

    add post & delete endpoints

--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import { SageMaker } from "@aws-sdk/client-sagemaker";
+import * as _ from 'lodash'
 
 dotenv.config();
 
@@ -9,24 +10,91 @@ const port = process.env.PORT || 3000;
 const region = process.env.REGION || "us-east-1";
 
 const sagemaker = new SageMaker({ region });
+const sagemakerMethods = getObjectMethods(sagemaker);
 
-app.get("/sagemaker", (_req, res) => {
-  let returnList = `<ul>`;
-  for (const method of getObjectMethods(sagemaker)) {
-    returnList += `<li>${method}</li>`;
+app.use(express.json())
+
+app.get("/sagemaker", (req, res) => {
+  const asList = req.query.asList === "true";
+  countMethodsByFirstWord(sagemakerMethods)
+  
+  if (asList) {
+    const returnList = arrayToHTMLList(sagemakerMethods);
+    res.send(returnList);
+    return;
   }
-  returnList += `</ul>`;
-  res.send(returnList);
+  
+  res.send(sagemakerMethods);
 });
 
 app.get("/sagemaker/:functionName", (req, res) => {
-  if (!isValidCommand(sagemaker, req.params.functionName)) {
+  const getMethods = sagemakerMethods.filter(
+    (methodName) =>
+      methodName.startsWith("get") ||
+      methodName.startsWith("list") ||
+      methodName.startsWith("describe")
+  );
+  if (!isValidCommand(getMethods, req.params.functionName)) {
     res.status(404).send(`${req.params.functionName} is not a valid endpoint.`);
     return;
   }
 
-  const FunctionName = req.params.functionName;
-  const params = req.query;
+  const FunctionName = _.camelCase(req.params.functionName);
+  const params = _.merge({}, req.query, req.body);
+  console.log(params);
+
+  sagemaker[FunctionName](params)
+    .then((data: any) => {
+      console.log(data);
+      res.send(data);
+    })
+    .catch((error: any) => {
+      console.log(error);
+      res.status(400).send(error);
+    });
+});
+
+app.post("/sagemaker/:functionName", (req, res) => {
+  const postMethods = sagemakerMethods.filter(
+    (methodName) =>
+    methodName.startsWith("add") ||
+      methodName.startsWith("create") ||
+      methodName.startsWith("update")
+  );
+  if (!isValidCommand(postMethods, req.params.functionName)) {
+    res.status(404).send(`${req.params.functionName} is not a valid endpoint.`);
+    return;
+  }
+
+  const FunctionName = _.camelCase(req.params.functionName);
+  const params = _.merge({}, req.query, req.body);
+
+  console.log(params);
+  
+  sagemaker[FunctionName](params)
+    .then((data: any) => {
+      console.log(data);
+      res.send(data);
+    })
+    .catch((error: any) => {
+      console.log(error);
+      res.status(400).send(error);
+    });
+});
+
+app.delete("/sagemaker/:functionName", (req, res) => {
+  const deleteMethods = sagemakerMethods.filter(
+    (methodName) =>
+      methodName.startsWith("delete") ||
+      methodName.startsWith("remove")
+  );
+  if (!isValidCommand(deleteMethods, req.params.functionName)) {
+    res.status(404).send(`${req.params.functionName} is not a valid endpoint.`);
+    return;
+  }
+
+  const FunctionName = _.camelCase(req.params.functionName);
+  const params = _.merge({}, req.query, req.body);
 
   sagemaker[FunctionName](params)
     .then((data: any) => {
@@ -43,20 +111,49 @@ app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
 
+function arrayToHTMLList(array: string[]) {
+  let returnList = `<ul>`;
+  for (const method of array) {
+    returnList += `<li>${method}</li>`;
+  }
+  returnList += `</ul>`;
+  return returnList;
+}
+
 function getObjectMethods(obj: any): string[] {
   let methods: string[] = [];
   let currentObj = obj;
 
   while (currentObj) {
-    const objMethods = Object.getOwnPropertyNames(currentObj)
-      .filter((prop) => typeof currentObj[prop] === "function");
+    const objMethods = Object.getOwnPropertyNames(currentObj).filter(
+      (methodName) =>
+        methodName !== "destroy" &&
+        methodName !== "send" &&
+        !getDefaultObjectMethods().includes(methodName) &&
+        typeof currentObj[methodName] === "function"
+    );
     methods = methods.concat(objMethods);
     currentObj = Object.getPrototypeOf(currentObj);
   }
   return methods;
 }
 
-function isValidCommand(client: Object, commandName: string): boolean {
-  const methods = getObjectMethods(client);
+function getDefaultObjectMethods(): string[] {
+  const defaultObj = Object.getPrototypeOf({});
+  return Object.getOwnPropertyNames(defaultObj);
+}
+
+function isValidCommand(methods: string[], commandName: string): boolean {
   return methods.includes(commandName);
+}
+
+function countMethodsByFirstWord(methods: string[]): Record<string, number> {
+  const result: Record<string, number> = {};
+  for (const method of methods) {
+      const firstWord = _.kebabCase(method).split('-')[0];
+      result[firstWord] = (result[firstWord] || 0) + 1;
+  }
+  
+  console.table(result);
+  return result;
 }
